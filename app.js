0e8f1d1643c00f5fd514b0a134d85f777075e308
Đặng Đình Quân <quandd.vnip@gmail.com>
2022-11-12 15:59:00 +0700

    add /givemecookie endpoint

--- app.js
+++ app.js
@@ -5,4 +5,9 @@ app.get('/', async (req, res) => {
     res.send("Welcome to homepage!");
 });
 
+app.get('/givemecookie', async (req, res) => {
+    res.cookie('sweet', 'sugar', { maxAge: 7 * 24 * 60 * 60 * 1000, httpOnly: true });
+    res.send("Cookie named 'sweet' has been set!");
+});
+
 app.listen(80);
